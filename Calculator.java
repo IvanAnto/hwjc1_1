353beeb8faaaa3193eda37914784e41d22af8375
Anton <antony679@mail.ru>
2021-09-28 21:41:24 +0300

    First commit

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,40 @@ import java.util.function.*;
 
 class Calculator {
   
-  private static Supplier<Integer> callback; // создаем статическую переменную (поле) типа Supplier. Функциональный интерфейс реализует метод get().
+  //private static Supplier<Integer> callback; 
   
-  public Calculator(Supplier<Integer> callback) {
-    this.callback = callback; 
-  }
-  
+  //public Calculator(Supplier<Integer> callback) {
+    //this.callback = callback; 
+  //}
+  static Supplier<Calculator> instance = Calculator::new; // создаем статическую переменную (поле) типа Supplier. Функциональный интерфейс Supplier реализует метод get(). В качестве класс выступает Calculator в конструктор которого ничего не передается.
+
   public Calculator(){}
+
+
+  BinaryOperator<Integer> plus = (x, y) -> x + y;
+  BinaryOperator<Integer> minus = (x, y) -> x - y;
+  BinaryOperator<Integer> multiply = (x, y) -> x * y;
+  BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+  UnaryOperator<Integer> pow = x -> x * x;
+  //UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+  UnaryOperator<Integer> abs = x -> { 
+    if (x > 0) {
+      return x;
+    } else if (x < 0) {
+      return x * -1; 
+    } 
+
+    try {
+     throw new Exception();
+    } catch (Exception e) {
+     throw new RuntimeException(e);
+    }
+ 
+  };
+
+  Predicate<Integer> isPositive = x -> x > 0;
+
+  Consumer<Integer> println = System.out::println;
+
 }
--- Main.java
+++ Main.java
@@ -3,19 +3,13 @@ import java.util.function.*;
 class Main {
   public static void main(String[] args) {
     
-    static Supplier<Calculator> instance = Calculator::new;
-    
     Calculator calc = Calculator.instance.get(); // создаем экземпляр класса Calculator через вызов статической переменной instance
-   // Calculator calc = new Calculator(instance);
-    //System.out.println(calc);
-    
-    BinaryOperator<Integer> plus = (x, y) -> x + y;
-    BinaryOperator<Integer> minus = (x, y) -> x - y;
-    BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
 
-    int a = calc.plus.apply(2, 1);
-    int b = calc.minus.apply(3, 1);
+    //System.out.println(Calculator.instance.get());
+    //System.out.println(calc);
+
+    int a = calc.plus.apply(1, 2);
+    int b = calc.minus.apply(1, 1);
     int c = calc.devide.apply(a, b);
     calc.println.accept(c);
 
